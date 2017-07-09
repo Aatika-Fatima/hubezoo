c3399f50e841eefc23ba3acdb443beed3822ee47
ebinezer <ebinezer@HYD-BBG-EBINEZER>
2017-07-09 13:37:35 +0530

    category table

---
+++ hubezooback/src/main/java/com/hubezoo/hubezooback/HubezoobackApplication.java
@@ -0,0 +1,12 @@
+package com.hubezoo.hubezooback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubezoobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubezoobackApplication.class, args);
+	}
+}
